2c168dac9c2bdf0508adffdd77e00a4731cac063
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2022-12-12 20:38:37 +0530

    added click.js
    js clicks and keyword disabled
    topbar for admin improved

---
+++ assets/js/click.js
@@ -0,0 +1,106 @@
+// disable right click
+document.addEventListener('contextmenu', event => event.preventDefault());
+// disable ctrl+shift+i
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+c
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+j
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+k
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'K'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+u
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+s
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'S'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+o
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'O'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+p
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'P'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+a
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'A'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+q
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'Q'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+e
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'E'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+shift+i
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+// disable ctrl+u
+document.onkeydown = function(e) {
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    } else {
+        return true;
+    }
+}
