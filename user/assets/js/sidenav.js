50a194749a78b4ead53c3e8936b2b54985b7a59f
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2022-12-13 13:18:11 +0530

    changes
    js password
    css up

--- user/assets/js/sidenav.js
+++ user/assets/js/sidenav.js
@@ -36,17 +36,20 @@ sidenav.innerHTML +=
 
 <div class="container-fluid d-flex flex-column p-0">
   <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
-    <div class="sidebar-brand-icon rotate-n-15"><i class="fas fa-laugh-wink"></i></div>
-    <div class="sidebar-brand-text mx-3"><span>Brand</span></div>
+    <div class="sidebar-brand-icon">
+    <img src="../assets/icon/android-chrome-192x192.png" style="height: 60px">
+    </div>
+    <div class="sidebar-brand-text mx-3"><span>Webex</span></div>
   </a>
   
   <hr class="sidebar-divider my-0">
   
   <ul class="navbar-nav text-light" id="accordionSidebar">
-    <li class="nav-item"><a class="nav-link nav-home" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
-    <li class="nav-item"><a class="nav-link nav-profile" href="profile.html"><i class="fas fa-user"></i><span>Profile</span></a></li>
+
+  <!--  <li class="nav-item"><a class="nav-link nav-home" href="index.html"><i class="fas fa-tachometer-alt"></i><span>Dashboard</span></a></li>
+  -->  <li class="nav-item"><a class="nav-link nav-profile" href="profile.html"><i class="fas fa-user"></i><span>Profile</span></a></li>
     <li class="nav-item"><a class="nav-link nav-project" href="project.html"><i class="fas fa-tachometer-alt"></i><span>Projects</span></a></li>
-    <li class="nav-item"><a class="nav-link nav-schedule" href="schedule.html"><i class="fas fa-table"></i><span>Schedule</span></a></li>
+    <li class="nav-item"><a class="nav-link nav-schedule" href="exam.html"><i class="fas fa-table"></i><span>Exam</span></a></li>
     <div class="dropdown no-arrow nav-item">
       <a class="dropdown-toggle nav-link nav-workshop" data-bs-toggle="dropdown" aria-expanded="false">
         <i class="fas fa-laptop"></i>
