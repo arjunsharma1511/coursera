74c369b6f9a5e6aa728e5ce73b87d2090e1b7f74
arjunsharma1511 <96876989+arjunsharma1511@users.noreply.github.com>
2022-12-14 16:58:18 +0530

    Update topnav.js

--- user/assets/js/topnav.js
+++ user/assets/js/topnav.js
@@ -99,7 +99,7 @@ div, p, span {color: #fff;}
                     <i class="fas fa-list fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Activity log
                 </a>
                   <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="">
+                <a class="dropdown-item" href="../index.html">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw me-2 text-gray-400"></i>&nbsp;Logout
                 </a>
               </div>
